fix computation of constraints over horizon

harmony_mpcs/mpc_planner/mpcPlanner.py
@@ -81,16 +81,23 @@ class MPCPlanner(object):
 
         self.setWeights()
 
-    def setX0(self, initialize_type="current_state", initial_step= True):
+    def setX0(self, initialize_type="current_state", initial_step= False):
         if initialize_type == "current_state" or initialize_type == "previous_plan" and initial_step:
             for i in range(self._N):
                 self._x0[i][self._nu:] = self._xinit
                 self._initial_step = False
         elif initialize_type == "previous_plan":
-            self.shiftHorizon(self.output)
+            self.shiftHorizon(self._output)
         else:
             np.zeros(shape=(self._N, self._nx + self._nu + self._ns))
 
+    def shiftHorizon(self, output):
+        for i in range(len(output)):
+            if i == 0:
+                continue
+            self._x0[i - 1, 0 : len(output[i,:])] = output[i,:]
+        self._x0[-1:, 0 : len(output[i,:])] = output[i,:]
+            
     def setWeights(self,update_goal_region=False):
         for N_iter in range(self._N):
             k = N_iter * self._npar

harmony_mpcs/utils/free_space_decomposition.py
@@ -82,15 +82,16 @@ class FreeSpaceDecomposition(object):
         self._lidar_2d = lidar_2d
 
     def set_position(self, position: np.ndarray):
-        self._position = position
+        if position.shape[0] == 2:
+            self.position = np.concatenate((position, np.zeros(1)))
+        else:
+            self._position = position
 
     def filter_point_cloud(self, points: np.ndarray):
         if self._lidar_2d:
             points = points[:,:2]
             self._position = self._position[:2]
-            #print('self._position', self._position, flush=True)
-        self._constraints = []
-        self._closest_points = []
+        
         dists = np.linalg.norm(points - self._position, axis=1)
         idx = np.argsort(dists)
         points = points[idx]
@@ -114,7 +115,12 @@ class FreeSpaceDecomposition(object):
         self,
         points: np.ndarray,
     ):
+        dists = np.linalg.norm(points - self._position, axis=1)
+        idx = np.argsort(dists)
+        points = points[idx]
         
+        self._constraints = []
+        self._closest_points = []
         while (
             points.size > 0
             and len(self._constraints) < self._number_constraints